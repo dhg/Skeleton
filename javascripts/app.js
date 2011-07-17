b63da767e898ceec84894b533efa50f11f7edf9a
Johan Brook <johan.jvb@gmail.com>
2011-07-17 17:19:19 +0200

    Removed 'maximum-scale=1.0' from meta tag. Mobile visitors should be able to zoom. Added JS fix for the iOS bug where the site will autozoom whenever the orientation is changed (see http://adactio.com/journal/4470/).

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,6 +9,22 @@
 	
 
 $(document).ready(function() {
+	
+	/* 	Prevent iPhone and iPad to autoscale the page when rotated 
+		(http://adactio.com/journal/4470/)
+	================================================================ */
+
+  	if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+   	  var viewportmeta = document.querySelector('meta[name="viewport"]');
+   	  if (viewportmeta) {
+   	    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+   	    document.body.addEventListener('gesturestart', function() {
+   	      viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+   	    }, false);
+   	  }
+   	}
+
+
 
 	/* Tabs Activiation
 	================================================== */
