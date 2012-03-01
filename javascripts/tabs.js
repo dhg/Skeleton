bc178a522844c21ee689ada53391197a34fc67c0
Guillermo Rauch <rauchg@gmail.com>
2012-03-01 19:53:51 -0300

    It now works before dom is ready :)

--- javascripts/tabs.js
+++ javascripts/tabs.js
@@ -8,21 +8,22 @@
  * 8/17/2011
  */
 
-jQuery(function ($) {
+(function ($) {
   // hash change handler
   function hashchange () {
     var hash = window.location.hash
       , el = $('ul.tabs [href*="' + hash + '"]')
+      , content = $(hash)
 
-    if (el.length) {
-      $(el).closest('.tabs').find('.active').removeClass('active');
-      $(el).addClass('active');
-
-      $(hash).show().addClass('active').siblings().hide().removeClass('active');
+    if (el.length && !el.hasClass('active') && content.length) {
+      el.closest('.tabs').find('.active').removeClass('active');
+      el.addClass('active');
+      content.show().addClass('active').siblings().hide().removeClass('active');
     }
   }
 
   // listen on event and fire right away
   $(window).on('hashchange.skeleton', hashchange);
   hashchange();
-});
+  $(hashchange);
+})(jQuery);
